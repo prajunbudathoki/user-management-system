4545b76e7a3a70d47e39a836c82131ae8ddaba71
prajun <prajunbudathoki1@gmail.com>
2025-04-17 11:34:16 +0000

    adding role based authenticaation

---
+++ src/api/auth.ts
@@ -0,0 +1,28 @@
+import type {User} from "../types/user"
+
+const testUser: User = {
+    id: 1,
+    email: 'hello@gmail.com',
+    role: 'employee'
+}
+
+export async function getUser() {
+    await new Promise((resolve) => setTimeout(resolve,1000))
+
+    const authToken = generateAuthToken()
+
+    return [200,{authToken, user:testUser}]
+}
+
+export async function login() {
+    await new Promise((resolve) => setTimeout(resolve,1000))
+
+    const authToken = generateAuthToken()
+
+    return [200, {authToken, user:testUser}]
+}
+
+
+function generateAuthToken() {
+    return Math.random().toString(36).substring(2)
+}
